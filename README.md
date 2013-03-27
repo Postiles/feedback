8d9484f0a0e4bd6cc48236e13cf9b35a821ca520
Kong Xiangzhou <kxzkxz7139@gmail.com>
2013-03-28 00:23:52 +0800

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+== HOW TO INSTALL ==
+
+Step 1. Pull all files into a directory
+
+Step 2. Go Chrome's extension page, say chrome://extensions. Check "Developer mode". Then "Load unpacked extension". Then select the folder which you pull the repo to.
---
+++ main.js
@@ -0,0 +1,28 @@
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+    if (tab.url.indexOf('postiles.com') > -1) {
+        chrome.pageAction.show(tabId);
+    }
+};
+
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.windows.getCurrent(function (win) {
+        chrome.tabs.captureVisibleTab(null, {"format":"png"}, function(imgUrl) {
+            chrome.tabs.executeScript(tab.id, { code: 'window.postMessage({ action: "SCREENSHOT", img: "'+imgUrl+'" }, "*")' });                 
+        });    
+    });    
+});
+
+function install_notice() {
+    if (localStorage.getItem('install_time')) {
+        return;
+    }
+        
+    var now = new Date().getTime();
+    localStorage.setItem('install_time', now);
+    alert("You've installed Postile's feedback extension. When visiting postiles, you'll see a icon on the right of your address bar. Click that to send a report. Thanks.");
+}
+
+install_notice();
